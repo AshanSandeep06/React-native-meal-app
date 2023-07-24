e2da23b3b5599dc29206c3eb6dc4e1207c7ad700
AshanSandeep06 <ashansandeep06@gmail.com>
2023-07-25 01:17:38 +0530

    Content Updated

---
+++ store/redux/Favorite_Redux.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native';
+
+const Favorite_Redux = () => {
+  return (
+    <View>
+        <Text>Hello There</Text>
+    </View>
+  );
+}
+
+export default Favorite_Redux;
