51988c1dd088c660eefd527bfb292172d82ab382
AshanSandeep06 <ashansandeep06@gmail.com>
2023-07-24 23:26:15 +0530

    adding Favorite_Context.js for refactor favorite items using Context-API

---
+++ store/context/Favorite_Context.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native';
+
+const Favorite_Context = () => {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+export default Favorite_Context;
