73f9bcc73aee65fbb72c59724e1fb2eed3ca20d5
AshanSandeep06 <ashansandeep06@gmail.com>
2023-07-25 00:59:47 +0530

    Content Updated with Content refactored with MealList Component

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -3,54 +3,13 @@ import { MEALS } from "../data/dummy-data";
 import { FlatList } from "react-native";
 import MealItem from "../components/MealList/MealItem";
 import { useNavigation } from "@react-navigation/native";
+import MealList from "../components/MealList/MealList";
 
 const MealsOverviewScreen = ({ route }) => {
-    const navigation = useNavigation();
     const { categoryId } = route.params;
-
     const displayMeals = MEALS.filter((mealItem) => mealItem.categoryIds.indexOf(categoryId) >= 0);
 
-    const renderMealItems = (itemData) => {
-        const item = itemData.item;
-
-        const pressHandler = () => {
-            // Navigate to Meal Details Screen
-            navigation.navigate("MealDetails", { mealId: item.id });
-        };
-
-            {/* Creating props of Meal Item */}
-            const mealItemProps = {
-                title: item.title,
-                imageUrl: item.imageUrl,
-                complexity: item.complexity,
-                affordability: item.affordability,
-                duration: item.duration,
-                onPress: pressHandler
-            };
-
-        return(
-            <View>
-                <MealItem {...mealItemProps} /> 
-            </View>
-        );
-    };
-
-    return (
-        <View style={styles.container}>
-            <FlatList
-                data={displayMeals}
-                keyExtractor={(item) => item.id}
-                renderItem={renderMealItems}
-            />
-        </View>
-    );
+    return <MealList itemData={displayMeals} />
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 16
-    }
-});
 
 export default MealsOverviewScreen;
