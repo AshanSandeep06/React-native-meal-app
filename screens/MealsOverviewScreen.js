b483d6b8bff34b37e89d38164bf7623dfe4bfdea
AshanSandeep06 <ashansandeep06@gmail.com>
2023-07-24 12:37:47 +0530

    Content Updated with MealsOverviewScreen Component

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,25 @@
 import { Text, View, StyleSheet } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import { FlatList } from "react-native";
 
 const MealsOverviewScreen = ({ route }) => {
     const { categoryId } = route.params;
 
+    const displayMeals = MEALS.filter((mealItem) => mealItem.categoryIds.indexOf(categoryId) >= 0);
+
+    const renderMealItems = (itemData) => {
+        return <View>
+            <Text>{itemData.item.title}</Text>
+        </View>
+    };
+
     return (
         <View style={styles.container}>
-            <Text>Meals Overview Screen {categoryId} </Text>
+            <FlatList
+                data={displayMeals}
+                keyExtractor={(item) => item.id}
+                renderItem={renderMealItems}
+            />
         </View>
     );
 }
