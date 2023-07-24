c1ebf43d1a4e10a5532ccce8e6338996b65e1b8d
AshanSandeep06 <ashansandeep06@gmail.com>
2023-07-25 01:16:38 +0530

    Content Refactored in FavoritesScreen.js

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,10 +1,38 @@
-import React from 'react'
-import { Text } from 'react-native';
+import React, { useContext } from 'react'
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FavoriteContext } from '../store/context/Favorite_Context';
+import { MEALS } from '../data/dummy-data';
+import MealList from '../components/MealList/MealList';
 
 const FavoritesScreen = props => {
+  const favoriteMealContext = useContext(FavoriteContext);
+  const allFavoriteMeals = MEALS.filter((meal) => favoriteMealContext.ids.includes(meal.id));
+
+  if(allFavoriteMeals.length === 0){
+    return(
+      <View style={styles.rootContainer}>
+          <Text style={styles.text}>No Favorite Meals Found, Start adding some!</Text>
+      </View>
+    );
+  }
+
   return (
-     <Text>The Favorites Screen</Text>
-  )
+     <MealList itemData={allFavoriteMeals} />
+  );
 }
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: 'center'
+  },
+
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white'
+  }
+});
 
 export default FavoritesScreen;
