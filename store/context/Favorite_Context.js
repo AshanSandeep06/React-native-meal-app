da5f83b9d02700fc917a8ae5f55a1790e106c8bc
AshanSandeep06 <ashansandeep06@gmail.com>
2023-07-25 00:06:43 +0530

    Fully Refactored Context-API on Favorite_Context.js file

--- store/context/Favorite_Context.js
+++ store/context/Favorite_Context.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View } from 'react-native';
 import { createContext } from 'react';
 
@@ -8,9 +8,31 @@ export const FavoriteContext = createContext({
     removeFavorite: (id) => {}
 });
 
+// Me children kyna prop eka auto ma pass wenwa,
+// FavoriteContextProvider kyna me component eken child components
+// Wrap krma.
 const FavoriteContextProvider = ({ children }) => {
+  // Managing State with React
+  const [favoriteMealIds, setFavoriteMealIds] = useState([]);
+
+  const addFavorite = (id) => {
+    setFavoriteMealIds((currentIds) => [...currentIds, id]);
+  };
+
+  const removeFavorite = (id) => {
+    setFavoriteMealIds((currentIds) => {
+      return currentIds.filter((mealId) => mealId !== id)
+    });
+  };
+
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
+
   return(
-    <FavoriteContext.Provider>
+    <FavoriteContext.Provider value={value}>
         {children}
     </FavoriteContext.Provider>
   );
