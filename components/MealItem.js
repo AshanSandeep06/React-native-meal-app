c076ce2e7a96c82a34f62063893b0d34f711d2be
AshanSandeep06 <ashansandeep06@gmail.com>
2023-07-24 14:29:16 +0530

    Refactoring MealItem Component

---
+++ components/MealItem.js
@@ -0,0 +1,68 @@
+import { View, Text, Image, StyleSheet, Platform, Pressable } from "react-native";
+
+const MealItem = ({ title, imageUrl, duration, complexity, affordability }) => {
+    return(
+        <View style={styles.MealItem}>
+            <Pressable android_ripple={{color: '#ccc'}}>
+
+                <View style={styles.innerContainer}>
+                    <View>
+                        <Image source={{ uri: imageUrl }} style={styles.image} />
+                        <Text style={styles.title}>{title}</Text>
+                    </View>
+
+                    <View style={styles.details}>
+                        <Text style={styles.detailItem}>{duration}</Text>
+                        <Text style={styles.detailItem}>{complexity}</Text>
+                        <Text style={styles.detailItem}>{affordability}</Text>
+                    </View>
+                </View>
+            </Pressable>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    MealItem: {
+        margin: 16,
+        borderRadius: 8,
+        overflow: Platform.OS === "android" ? 'hidden' : 'visible',
+        backgroundColor: "white",
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset:{ width: 0, height: 2},
+        shadowRadius: 8,
+    },
+
+    innerContainer: {
+        borderRadius: 8,
+        overflow: 'hidden'
+    },
+
+    image: {
+        width: '100%',
+        height: 200
+    },
+    
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        textAlign: 'center',
+        margin: 8
+    },
+
+    details: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 8,
+        justifyContent: 'center'
+    },
+
+    detailItem: {
+        marginHorizontal: 4,
+        fontSize: 12
+    }
+});
+
+export default MealItem;
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,6 +1,7 @@
 import { Text, View, StyleSheet } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import { FlatList } from "react-native";
+import MealItem from "../components/MealItem";
 
 const MealsOverviewScreen = ({ route }) => {
     const { categoryId } = route.params;
@@ -8,9 +9,18 @@ const MealsOverviewScreen = ({ route }) => {
     const displayMeals = MEALS.filter((mealItem) => mealItem.categoryIds.indexOf(categoryId) >= 0);
 
     const renderMealItems = (itemData) => {
-        return <View>
-            <Text>{itemData.item.title}</Text>
-        </View>
+        const item = itemData.item;
+
+            {/* Creating props of Meal Item */}
+            const mealItemProps = {
+                title: item.title,
+                imageUrl: item.imageUrl,
+                complexity: item.complexity,
+                affordability: item.affordability,
+                duration: item.duration
+            };
+
+        return <View> <MealItem {...mealItemProps} /> </View>
     };
 
     return (
