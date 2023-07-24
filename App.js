6f047cc36a3b4ecc3293c248436e71ff9c661f10
AshanSandeep06 <ashansandeep06@gmail.com>
2023-07-24 11:17:58 +0530

    Added Native stack navigators with Screens for navigations

--- App.js
+++ App.js
@@ -2,15 +2,25 @@ import {StatusBar} from 'expo-status-bar';
 import { useState } from 'react';
 import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+
 import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Create Native Stack Navigator
+const Stack = createNativeStackNavigator();
 
 export default function App() {
     return (
         <>
             {/* Status Bar eka light wenwa */}
             <StatusBar style='light' />
+
             <NavigationContainer>
-                <CategoriesScreen />
+                {/* Me Native stack navigator ekata, Screens tika denna ona */}
+                <Stack.Navigator>
+                    {/* name --> kyna prop eka magin me screen ekata header ekk set wenwa */}
+                    <Stack.Screen name="Meals Categories" component={CategoriesScreen} />
+                </Stack.Navigator>
             </NavigationContainer>
         </>
     );
